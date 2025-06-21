b7041fdf16e7fe39614fb825165b78d76ab0b5e1
Siddharth TR <siddartha.24.beai@acharya.ac.in>
2025-06-21 15:45:51 +0530

    Create coin_toss_debugged.py

---
+++ coin_toss_debugged.py
@@ -0,0 +1,23 @@
+import random
+#coin_toss_debugged.py
+#📄 Description:
+#Fixes a buggy coin toss guessing game where the user gets two chances to guess correctly.
+print("name : Siddarth TR \n usn : 1AY24AI106 \n section : O ")
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+
+toss = random.randint(0, 1)
+toss = 'heads' if toss == 1 else 'tails'
+
+if toss == guess:
+    print('You got it!')
+else:
+    
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
